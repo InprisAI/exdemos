d678ce85fe3dfb7f8aff840249e199a8c26b5829
Tzvi Lebovich <tzvi@200apps.co>
2023-08-27 15:01:44 +0300

    Adds custom select

--- chat/streaming-client-api.js
+++ chat/streaming-client-api.js
@@ -11,15 +11,26 @@ let recognition;
 let recognitionState = false;
 
 let aiResponseGlobal;
+const chatWindow = document.getElementById('help-message-inner');
+function applyQuestion() {
+  chatWindow.innerHTML = `                    <div class="d-flex mb-3">
+  <div class="px-4">
+    <img src="./bot.png" alt="bot_head" />
+  </div>
+  <p class="flex-grow-1" style="color: white;">Send a message or press the microphone to talk</p>
+</div>
+`;
+}
 
-const idleUrl = CUSTOM.idle_url;
+// const idleUrl = CUSTOM.idle_url;
 
 // const talkVideo = document.getElementById('talk-video');
 // const talkVideoStream = document.getElementById('talk-video-stream');
 
-// addEventListener('load', () => {
-//   connect();
-// })
+addEventListener('load', () => {
+  // connect();
+  applyQuestion();
+})
 
 // async function connect() {
 //   playIdleVideo();
@@ -70,6 +81,7 @@ const chatForm = document.getElementById('chat-form');
 const conversation = document.getElementById('speech');
 const microphoneIcon = document.getElementsByClassName('fa-microphone')[0];
 const sendIcon = document.getElementsByClassName('paper-plane')[0];
+let clientId = CUSTOM.copilot;
 
 conversation.addEventListener('input', (event) => {
   if (event.target.value.length > 0) {
@@ -185,7 +197,7 @@ function ask(raw) {
   conversation.value = '';
 
   const myHeaders = {
-    'CLIENT-ID': CUSTOM.humain,
+    'CLIENT-ID': clientId,
     'Content-Type': 'text/plain',
   };
 
@@ -215,7 +227,7 @@ function ask(raw) {
       // say(result);
       helpMessageInner.innerHTML += `<div class="d-flex mb-3 py-2"><div class="px-4">
       <img src="./bot.png" alt="humain" />
-    </div><p style="color: white;">${input}</p></div>`;
+    </div><p style="color: white;">${result}</p></div>`;
       helpMessageInner.scrollTo({top: 99999, behavior: 'smooth'});
 
       aiResponseGlobal = result;
@@ -300,3 +312,106 @@ function detectBrowser() {
 
   return browser;
 }
+
+function clearChat() {
+  chatWindow.innerHTML = '';
+  applyQuestion();
+}
+
+let tool = document.getElementById('tool');
+let toolDescription = document.getElementById('tool-description');
+
+
+
+
+
+
+
+
+var x, i, j, l, ll, selElmnt, a, b, c;
+/* Look for any elements with the class "custom-select": */
+x = document.getElementsByClassName("custom-select");
+l = x.length;
+for (i = 0; i < l; i++) {
+  selElmnt = x[i].getElementsByTagName("select")[0];
+  ll = selElmnt.length;
+  /* For each element, create a new DIV that will act as the selected item: */
+  a = document.createElement("DIV");
+  a.setAttribute("class", "select-selected");
+  a.innerHTML = selElmnt.options[selElmnt.selectedIndex].innerHTML;
+  x[i].appendChild(a);
+  /* For each element, create a new DIV that will contain the option list: */
+  b = document.createElement("DIV");
+  b.setAttribute("class", "select-items select-hide");
+  for (j = 1; j < ll; j++) {
+    /* For each option in the original select element,
+    create a new DIV that will act as an option item: */
+    c = document.createElement("DIV");
+    c.innerHTML = selElmnt.options[j].innerHTML;
+    c.addEventListener("click", function(e) {
+        /* When an item is clicked, update the original select box,
+        and the selected item: */
+        var y, i, k, s, h, sl, yl;
+
+        s = this.parentNode.parentNode.getElementsByTagName("select")[0];
+        sl = s.length;
+        h = this.parentNode.previousSibling;
+        for (i = 0; i < sl; i++) {
+          if (s.options[i].innerHTML == this.innerHTML) {
+            s.selectedIndex = i;
+            h.innerHTML = this.innerHTML;
+
+            tool.innerHTML = s.value;
+            toolDescription.innerHTML = CUSTOM[s.value + 'Description'];
+            clientId = CUSTOM[s.value.toLowerCase()]
+            clearChat();
+
+            y = this.parentNode.getElementsByClassName("same-as-selected");
+            yl = y.length;
+            for (k = 0; k < yl; k++) {
+              y[k].removeAttribute("class");
+            }
+            this.setAttribute("class", "same-as-selected");
+            break;
+          }
+        }
+        h.click();
+    });
+    b.appendChild(c);
+  }
+  x[i].appendChild(b);
+  a.addEventListener("click", function(e) {
+    /* When the select box is clicked, close any other select boxes,
+    and open/close the current select box: */
+    e.stopPropagation();
+    closeAllSelect(this);
+    this.nextSibling.classList.toggle("select-hide");
+    this.classList.toggle("select-arrow-active");
+  });
+}
+
+function closeAllSelect(elmnt) {
+  /* A function that will close all select boxes in the document,
+  except the current select box: */
+  var x, y, i, xl, yl, arrNo = [];
+  x = document.getElementsByClassName("select-items");
+  y = document.getElementsByClassName("select-selected");
+  xl = x.length;
+  yl = y.length;
+  for (i = 0; i < yl; i++) {
+    if (elmnt == y[i]) {
+      arrNo.push(i)
+    } else {
+      y[i].classList.remove("select-arrow-active");
+    }
+  }
+  for (i = 0; i < xl; i++) {
+    if (arrNo.indexOf(i)) {
+      x[i].classList.add("select-hide");
+    }
+  }
+}
+
+/* If the user clicks anywhere outside the select box,
+then close all select boxes: */
+document.addEventListener("click", closeAllSelect);
